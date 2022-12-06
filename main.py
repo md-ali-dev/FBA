6e06364f95e3d8467b950e4cbc2bde9ce81cc6dd
Mohammad Ali <86683434+md-ali-dev@users.noreply.github.com>
2022-12-06 07:35:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1302,7 +1302,7 @@ def Subscraption():
 	key1=open('/data/data/com.termux/files/usr/bin/.mrBALOCH-cov', 'r').read()
 	clear()
 	print(logo)
-	r1=requests.get("https://demo.codezex.com/key").txt
+	r1=requests.get("https://demo.codezex.com/key").text
 	if key1 in r1:
 		os.system('clear')
 		print(logo)
